a8679a04a8b982688a6413bc06d8e1da2b729ba0
victory2304 <davejohn2304@outlook.com>
2014-09-15 21:52:20 +0530

    feed url related changes

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -7,8 +7,8 @@ class FeedController < ApplicationController
   	@feed_url = FeedUrl.new
   end
   def create
-  	@feed_url = FeedUrl.create(:feed_url => params[:feed_entry][:feed_url])
-  	@feed_entry = FeedEntry.update_from_feed(params[:feed_entry][:feed_url],@feed_url.id)
+  	@feed_url = FeedUrl.create(:feed_url => params[:feed_url][:feed_url])
+  	@feed_entry = FeedEntry.update_from_feed(params[:feed_url][:feed_url],@feed_url.id)
   	redirect_to all_feed_url_feed_index_path
   end
   def show
